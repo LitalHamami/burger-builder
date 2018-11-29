05e1a4294e198872372a992485813c8874a36729
Lital Hamami <lital.hamami@gmail.com>
2018-11-29 12:22:37 +0200

    Updates

---
+++ precache-manifest.6406143abde6c31afc87559189f315c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "fb5fd24da2a77ad9b1cd",
+    "url": "/burger-builder/static/js/runtime~main.fb5fd24d.js"
+  },
+  {
+    "revision": "d33b73ca32d86de2b28a",
+    "url": "/burger-builder/static/js/main.d33b73ca.chunk.js"
+  },
+  {
+    "revision": "3ea2d5b39465ac6acd8b",
+    "url": "/burger-builder/static/js/1.3ea2d5b3.chunk.js"
+  },
+  {
+    "revision": "d33b73ca32d86de2b28a",
+    "url": "/burger-builder/static/css/main.f5c3d058.chunk.css"
+  },
+  {
+    "revision": "64dc6828425003076aa6f96eb3cb1ac8",
+    "url": "/burger-builder/index.html"
+  }
+];
